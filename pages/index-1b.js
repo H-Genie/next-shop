f63877bb39c72d5730f3b9bee894efbc1459a7a2
h-genie <ses2000hj@hanmail.net>
2024-07-23 19:58:38 +0900

    198. Incremental Static Regeneration

---
+++ pages/index-1b.js
@@ -0,0 +1,29 @@
+// Option 1b: fetch products on the server side (in getStaticProps)
+import Head from "next/head"
+import Title from "../components/Title"
+import { getProducts } from "../lib/products"
+
+export const getStaticProps = async () => {
+  console.log("[HomePage] getStaticProps()")
+  const products = await getProducts()
+  return { props: { products } }
+}
+
+export default function HomePage({ products }) {
+  console.log("🚀 ~ products:", products)
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="px-6 py-4">
+        <Title children={"Next Shop"} />
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  )
+}
