493b1d439afc39d04687070e7767805fbedcdfc7
h-genie <ses2000hj@hanmail.net>
2024-08-05 21:52:05 +0900

    221. Sign In API 요청

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -3,14 +3,20 @@ import Button from "../components/Button"
 import Field from "../components/Field"
 import Input from "../components/Input"
 import Page from "../components/Page"
+import { fetchJson } from "../lib/api"
 
 export default function SigninPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
-    console.log("should submit:", { email, password })
+    const response = await fetchJson("http://localhost:4000/auth/local", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ identifier: email, password })
+    })
+    console.log("sign in:", response)
   }
 
   return (
