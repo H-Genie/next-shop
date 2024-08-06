a352342293fa1868f967a556de0251575bcf0bc5
h-genie <ses2000hj@hanmail.net>
2024-08-06 20:51:59 +0900

    223. POST를 사용한 API 라우트

---
+++ pages/api/login.js
@@ -0,0 +1,8 @@
+export default function handleLogin(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).end()
+    return
+  }
+  console.log("req.body:" + req.body)
+  res.status(200).json({})
+}
