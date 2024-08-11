4f55a43b1f8ffeb5b84bff50351d7b3145bc3eda
h-genie <ses2000hj@hanmail.net>
2024-08-11 18:26:50 +0900

    226. 인증이 있는 NavBar

---
+++ components/NavBar.js
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+export default function NavBar() {
+  const user = { name: "Alice" }
+
+  return (
+    <nav className="px-2 py-1 text-sm">
+      <ul className="flex gap-2">
+        <li className="text-lg font-extrabold">
+          <Link href={"/"}>Next Shop</Link>
+        </li>
+        <li role="separator" className="flex-1" />
+        {user ? (
+          <>
+            <li>{user.name}</li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link href={"/sign-in"}>Sign In</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
